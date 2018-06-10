e3c589a6e87c7dcb35d804f6cc93bab2e6ad50ce
kosc2000 <kosc2000@hotmail.com>
2018-06-10 08:05:10 +0800

    增加一些空文件填充空的包，不然空包不被提交

---
+++ demo-service-root/demo-service/src/main/java/com/demo/workflow/dao/impl/DemoDaoImpl.java
@@ -0,0 +1,8 @@
+package com.demo.workflow.dao.impl;
+
+import com.demo.workflow.dao.DemoDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DemoDaoImpl implements DemoDao {
+}
